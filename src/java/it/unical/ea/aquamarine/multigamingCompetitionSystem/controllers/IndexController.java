72437854aeb30fe51808e82fc3134cc56fa25661
Denise <Denise@DAngel>
2014-12-03 16:05:28 +0100

    Spring introduced, repository moved to the project folder

---
+++ src/java/it/unical/ea/aquamarine/multigamingCompetitionSystem/controllers/IndexController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unical.ea.aquamarine.multigamingCompetitionSystem.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class IndexController {
+	
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		return "index";
+	}
+	
+	
+}
---
+++ src/java/it/unical/ea/aquamarine/multigamingCompetitionSystem/utilities/User.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unical.ea.aquamarine.multigamingCompetitionSystem.utilities;
+
+/**
+ *
+ * @author Denise
+ */
+public class User {
+	private String username;
+	private String password;
+	
+	public User(){
+		username = new String("");
+		password = new String("");
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+}
